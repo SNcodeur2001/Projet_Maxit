268bc61b8bf1d69f11fce6a72d449b31f3676515
SNcondeur2001 <mapathendiaye542@gmail.com>
2025-07-28 16:09:49 +0000

    render 1

--- app/config/env.php
+++ app/config/env.php
@@ -1,13 +1,18 @@
 <?php
 // config/env.php
 
-// require_once __DIR__ . '/../../vendor/autoload.php';
-
 use Dotenv\Dotenv;
 
-$dotenv = Dotenv::createImmutable(__DIR__ . '/../../');
-$dotenv->load();
+// On vérifie si le fichier .env existe (cas local uniquement)
+$envPath = dirname(__DIR__, 2) . '/.env';
 
+if (file_exists($envPath)) {
+    require_once dirname(__DIR__, 2) . '/vendor/autoload.php';
+    $dotenv = Dotenv::createImmutable(dirname(__DIR__, 2));
+    $dotenv->load();
+}
+
+// Variables base de données
 define('DB_DRIVER', $_ENV['DB_DRIVER'] ?? 'mysql');
 define('DB_HOST', $_ENV['DB_HOST'] ?? 'localhost');
 define('DB_NAME', $_ENV['DB_NAME'] ?? '');
@@ -16,6 +21,7 @@ define('DB_USER', $_ENV['DB_USER'] ?? '');
 define('DB_PASSWORD', $_ENV['DB_PASSWORD'] ?? '');
 define('DB_PATH', $_ENV['DB_PATH'] ?? '');
 define('DSN', $_ENV['DSN'] ?? DB_DRIVER . ":host=" . DB_HOST . ";dbname=" . DB_NAME);
+
 // Configuration Twilio
 define('TWILIO_SID', $_ENV['TWILIO_SID'] ?? '');
 define('TWILIO_TOKEN', $_ENV['TWILIO_TOKEN'] ?? '');
